a3ed64f1b605062b7efa47c7cc955c499c027698
witold <witold@home>
2021-06-27 22:44:41 +0200

    cleanup emacs plugins + add neotree patch

--- setup/emacs-plugins.sh
+++ setup/emacs-plugins.sh
@@ -2,10 +2,7 @@ git clone  https://github.com/winterTTr/ace-jump-mode.git
 git clone  https://github.com/company-mode/company-mode.git
 git clone  https://github.com/magnars/dash.el.git
 git clone  https://github.com/jaypei/emacs-neotree.git
-git clone  https://github.com/peterwang/folding.el.git
 git clone  https://github.com/nschum/highlight-symbol.el.git
 git clone  https://github.com/magnars/multiple-cursors.el.git
 git clone  https://github.com/emacsmirror/openwith.git
 git clone  https://github.com/milkypostman/powerline.git
-git clone  https://github.com/magnars/s.el.git
-git clone  https://github.com/emacsmirror/xclip.git
